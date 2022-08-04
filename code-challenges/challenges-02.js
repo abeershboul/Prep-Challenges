2caa1b2d57b808d11ff1905a8f0aec085901d9cd
abeershboul <abeer.shnoul@yahoo.com>
2022-08-04 15:19:36 +0300

    new

---
+++ code-challenges/challenges-02.js
@@ -0,0 +1,48 @@
+"use strict";
+// -------------------------------------------------------------------------------------------------------
+//  Challenge 01:
+//  Required:
+//
+//  Write a function that takes an array and returns the reversed array.
+//  Without using built-in methods.
+//  Input: ['C#', 'JS', 'Ruby','Python']
+//  Output: ['Python','Ruby','JS','C#']
+
+const reverseArray = (arr) => {
+  // write your code here
+  let arr2=[];
+  for(let i =arr.length-1;i>=0;i--)
+  {
+arr2.push(arr[i]);
+return arr2
+  }
+};
+
+// -------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------
+
+//  Challenge 2:
+// Required:
+
+//  Write a function that takes an array of numbers and increases its values by 10
+//
+//  Input: [20, 54, 89, 41]
+//  Output: [30, 64, 99, 51]
+
+const arrInc = (arr) => {
+
+  const result = [];
+  let a=0;
+  for(let x=0;x<arr.length;x++)
+  {
+a=arr[x]+10;
+result.push(a);
+
+  }
+  // write your code here
+  return result;
+};
+
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { reverseArray, arrInc };
